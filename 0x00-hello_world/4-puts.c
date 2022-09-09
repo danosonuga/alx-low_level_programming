63e650987fddfee46c8c13f340f2ccccd32757a3
daniel <osonuga.daniel@gmail.com>
2022-09-08 18:29:12 -0700

    ADD: 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * put_string - prints a string to the terminal
+ *
+ * Return: Always 0 (success)
+ */
+int put_string()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
