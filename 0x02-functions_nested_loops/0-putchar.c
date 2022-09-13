2d03551f7778cf26fa8fb990b664868506e2d8dd
daniel <osonuga.daniel@gmail.com>
2022-09-13 13:38:35 -0700

    ADD: 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 10);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The bested Loops task by ALX
