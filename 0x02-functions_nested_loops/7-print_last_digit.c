394b82c5981d94477a27c2b98b6483ac963717ed
daniel <osonuga.daniel@gmail.com>
2022-09-13 15:52:33 -0700

    ADD: 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of the parameter
+ * @n: The parameter of the function print_last_digit
+ *
+ * Return: Always 0 (success)
+ */
+int print_last_digit(int n)
+{
+	int lastdigit = n % 10;
+
+	_putchar(lastdigit + '0');
+
+	return (0);
+}
