e155212b69e8aa0e966b557676ac0a5f2e968cd3
daniel <osonuga.daniel@gmail.com>
2022-09-13 15:41:40 -0700

    ADD: 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * _abs - Computes the abosulte value of the parameter
+ * @n: The parameter to be computed
+ *
+ * Return: The absolute parameter
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
