b93817b1963e79b793b48ae6aca2bbd90fe87c8c
daniel <osonuga.daniel@gmail.com>
2022-09-13 16:42:09 -0700

    ADD: 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,20 @@
  */
 void jack_bauer(void)
 {
-	int hours = 0, minutes = 0;
+	int hours = 0, minutes;
 
 	while (hours < 24)
 	{
+		minutes = 0;
+
 		while (minutes < 60)
 		{
-			_putchar((hours/10) + '0');
-			_putchar((hours%10) + '0');
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
 			_putchar(':');
-			_putchar((minutes/10) + '0');
-			_putchar((minutes%10) + '0');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
 
 			minutes++;
 		}
