92dd763213e83e2e0f6793a0b0abb98d69086c6e
daniel <osonuga.daniel@gmail.com>
2022-09-13 18:42:06 -0700

    ADD: 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - adds two integers together
+ * @num1: The first parameter
+ * @num2: The second parameter
+ *
+ * Return: The result of the addition
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+}
