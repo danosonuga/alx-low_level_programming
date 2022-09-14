d8d933bc86a5dbd468e09c3ec6b685f3a2621833
daniel <osonuga.daniel@gmail.com>
2022-09-13 19:51:39 -0700

    ADD: 11-print_to_98.c file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,12 +18,16 @@ void print_to_98(int n)
 		}
 		printf("\n");
 	}
-	else
+	else if (n <= 98)
 	{
 		while (n < 98)
 		{
 			printf("%d, ", n++);
 			n++;
 		}
+	}
+	else
+	{
+		printf("%d", n);
 	}
 }
