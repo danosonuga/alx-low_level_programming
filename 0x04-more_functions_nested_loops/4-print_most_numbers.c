4e8a225a7bb88e41917c551b32acc635b3517c24
daniel <osonuga.daniel@gmail.com>
2022-09-15 12:07:24 -0700

    ADD: 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints all numbers except 2 and 4
+ *
+ * Return: Nothing
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		if (num == 2)
+		{
+			continue;
+		}
+		else if (num == 4)
+		{
+			continue;
+		}
+		_putchar(num + '0');
+	}
+}
