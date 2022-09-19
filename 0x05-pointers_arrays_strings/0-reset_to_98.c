9a6c9428038b66a442e62e1549ad02bb2504e664
daniel <osonuga.daniel@gmail.com>
2022-09-19 03:05:16 -0700

    ADD: README, 0-reset & main.h files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the initial n value from 402 to 98
+ * @n: pointer vai=riable
+ *
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
