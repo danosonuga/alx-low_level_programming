b58c2692bc3adf51c4b5904af8bfd6bea37a76fa
daniel <osonuga.daniel@gmail.com>
2022-09-19 03:20:54 -0700

    UPDATE: main.h file, ADD: 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of the intergers a and b
+ * @a: the first parameter to be swapped
+ * @b: the second parameter to be swapped
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
