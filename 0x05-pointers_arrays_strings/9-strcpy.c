f1a636b352923aac5c676819c3e6d1797afe70b5
daniel <osonuga.daniel@gmail.com>
2022-09-19 16:19:59 -0700

    ADD: 9-strcpy.c, UPDATE: main.h files

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string pointed to by src including \0
+ * @dest: the destination to copy to
+ * @src: the source of the copy
+ * Return: char dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	while (src[i] != '\0')
+	{
+		i++;
+		dest[i] = src[i];
+	}
+	return (dest);
+}
