14c67da483448a3f795170ba9e56cf0a9b27dbce
daniel <osonuga.daniel@gmail.com>
2022-09-22 04:18:53 -0700

    ADD: 1-strncat.c, UPDATE: main.h files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings by using
+ * 		n bytes from src.
+ * @dest: The parameter storing the string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	i = 0;
+	j = 0;
+
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
+	return (dest);
+}
