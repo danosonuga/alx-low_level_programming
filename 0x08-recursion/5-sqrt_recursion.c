8570a0f025705af9a46b863e05a3d66340e35c61
daniel <osonuga.daniel@gmail.com>
2022-09-28 17:37:46 -0700

    ADD: 5-sqrt_recursion.c, UPDATE: main.h files

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Prints the natural square root of a number
+ * @n: The Number
+ *
+ * Return: The Natural quare root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (_sqrt_recursion(n ** (1/2)));
+}
