5f1acafc188c4aca743d79166386b4039cd8b4a5
daniel <osonuga.daniel@gmail.com>
2022-09-30 14:53:09 -0700

    First Dir commit y'all!

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: The number of commands entered
+ * @argv: The array of commands with size
+ *
+ * Return: 0 (success)
+ */
+int main(int argc __attribute__((unused)), int *argv[]):
+	printf("%s\n", argv[0]);
+	return (0);
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+# 0x0A Argc and Argv
+
+## Tasks
+1 - Write a program that prints its name, followed by a new line
