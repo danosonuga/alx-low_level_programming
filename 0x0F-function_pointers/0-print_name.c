55214a7bd56e2504ee4139cd3d863e3fdaa367e2
daniel <osonuga.daniel@gmail.com>
2022-10-12 13:05:09 -0700

    ADD: README, HEADER FILE & FIRST TASK

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name given
+ * @name: the name parameter
+ * @f: the pointer to a funtion
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+# 0X0F Function Pointers ALX Task
+
+## Tasks
+1. Write a function that prints a name.
